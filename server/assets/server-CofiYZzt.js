8595d48057949bc79035f2de7c85f36c9e8f04eb
Michael Taye <michaeltaye200@gmail.com>
2026-06-03 05:51:28 +0300

    Update MORNING - SERVICES

--- server/assets/server-CofiYZzt.js
+++ server/assets/server-CofiYZzt.js
@@ -73,7 +73,7 @@ function getResponse() {
 }
 var HEADERS = { TSS_SHELL: "X-TSS_SHELL" };
 async function getStartManifest(matchedRoutes) {
-  const { tsrStartManifest } = await import("./_tanstack-start-manifest_v-C7t0Yeke.js");
+  const { tsrStartManifest } = await import("./_tanstack-start-manifest_v-R-5s_zr0.js");
   const startManifest = tsrStartManifest();
   let routes = startManifest.routes;
   routes[rootRouteId];
@@ -1146,8 +1146,8 @@ var getBaseManifest = getProdBaseManifest;
 var createEarlyHintsForRequest = createEarlyHintsCollector;
 async function loadEntries() {
   const [routerEntry, startEntry, pluginAdapters] = await Promise.all([
-    import("./router-Br_81cvm.js"),
-    import("./start-DtwlAEsj.js"),
+    import("./router-CYKGH8kk.js"),
+    import("./start-CrUona8z.js"),
     import("./empty-plugin-adapters-BFgPZ6_d.js")
   ]);
   return {
--- server/assets/start-CrUona8z.js
+++ server/assets/start-CrUona8z.js
@@ -1,4 +1,4 @@
-import { c as createMiddleware } from "./server-C1fQKnq_.js";
+import { c as createMiddleware } from "./server-CofiYZzt.js";
 import { r as renderErrorPage } from "../server.js";
 import "node:async_hooks";
 import "h3-v2";
